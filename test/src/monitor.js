2868108277db073d9f388d3724459f5d1a296900
wangshubin <876213774@qq.com>
2024-04-29 18:45:31 +0800

    :art: 整理代码

--- test/src/monitor.js
+++ test/src/monitor.js
@@ -3,6 +3,7 @@ import performance from '@front-monitor/performance';
 import recordScreen from '@front-monitor/recordscreen';
 
 frontMonitor.init({
+  // disabled: true,
   dsn: 'http://localhost:8083/reportData', // 上报的地址
   apikey: '4cd90ffb265e44228319f6b83079924d', // 项目唯一的id
   // userId: '18611436666', // 用户id
